5cf9fc89bad2e1b54ca86096e98062205b8c3bb0
anttiluode <67085641+anttiluode@users.noreply.github.com>
2026-08-21 08:16:45 +0300

    Expose BlockNeuron primitives

---
+++ blockneuron/__init__.py
@@ -0,0 +1,21 @@
+from .core import (
+    BlockNeuronLayer,
+    EdgeSpec,
+    FourModeBlock,
+    HyperLinearAttacker,
+    UnconditionedLinear,
+    effective_conductance,
+    mode_gain,
+    phase_gain,
+)
+
+__all__ = [
+    "BlockNeuronLayer",
+    "EdgeSpec",
+    "FourModeBlock",
+    "HyperLinearAttacker",
+    "UnconditionedLinear",
+    "effective_conductance",
+    "mode_gain",
+    "phase_gain",
+]
